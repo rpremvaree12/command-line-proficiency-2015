16f4a342dc6e7eb7844759eb2c5ff716d7dc9575
boccca2014 <Boccca2014@gmail.com>
2015-11-17 15:27:04 +0000

    Finished Test

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -1,14 +1,17 @@
 #create a string and store it in a variable
-
+stuff= "Hello world"
+def variable(stuff)
+    "#{stuff}! I'm using ruby."
+end
 #print out the value of the variable you just created
-
+puts variable(stuff)
 #print the sum of 185 and 14
-
+puts 185 + 14
 #print the product of 1.8 and 3.6
-
+puts 1.8 * 3.6
 #store the number 78 as a variable
-
+number = 78
 #print the average of the following numbers: 78, 55, 63, 98
-
+puts (78 + 55 + 63 + 98) / 4
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+# puts "When you run this file, this sentence should not appear on the terminal."
