69f8d2199246a1aea1fddc46f5af5bc7bb73faca
thecleverwolf <ryan.m.bonilla@gmail.com>
2015-11-17 15:43:05 +0000

    finished ryan bonilla

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -1,14 +1,20 @@
 #create a string and store it in a variable
+String first_name ="Ryan"; 
+String last_name ="Bonilla";
+name = first_name,last_name
+puts (first_name + " " +  last_name)
 
 #print out the value of the variable you just created
+puts ("My name is #{first_name} #{last_name}")
 
 #print the sum of 185 and 14
-
+puts (185+14)
 #print the product of 1.8 and 3.6
-
+puts (1.8*3.6)
 #store the number 78 as a variable
 
 #print the average of the following numbers: 78, 55, 63, 98
+puts (78+55+63+98)/4
 
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+#puts "When you run this file, this sentence should not appear on the terminal."
