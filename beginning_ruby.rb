54c8e987c1fbc915ed01bf0ff36703e2f02bd1d0
deniskabiesia <gdenise02@gmail.com>
2015-11-17 18:23:04 +0000

    pull request

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -1,16 +1,18 @@
 #create a string and store it in a variable
-
+y= "pizza"
 #print the value of the variable you just created
-
+x= " I like to eat #{y}"
+puts x
 #Use ruby to calculate and print the sum of 185 and 14
-
+puts(185 + 140)
 #Use ruby to calculate and print the product of 1.8 and 3.6
-
+puts(1.8 * 3.6)
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
-
+puts(155 + 65)/(3.2)
 #store the number 78 as a variable
-
+m=78 
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
+puts average(78,55,63,98)
 
 #comment out the following line
 puts "When you run this file, this sentence should not appear on the terminal."
