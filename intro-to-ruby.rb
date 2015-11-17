159af3c0d1a1f28374d7b3d4677650f8696b7c88
zenzile <tonge1@ms324.org>
2015-11-17 15:34:10 +0000

    Hello

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -1,14 +1,20 @@
 #create a string and store it in a variable
+food = "chicken,french fries,pizza"
+puts " like to eat #{food} after school."
 
 #print out the value of the variable you just created
 
+
 #print the sum of 185 and 14
+puts (185+14)
 
 #print the product of 1.8 and 3.6
-
+puts (1.8*3.6)
 #store the number 78 as a variable
+x="78"
 
 #print the average of the following numbers: 78, 55, 63, 98
+puts add 78,55,63,98
 
 #comment out the following line
 puts "When you run this file, this sentence should not appear on the terminal."
