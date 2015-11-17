28d8db109e3a946abd604bed354d53ad0611b3ad
vaniamaciel99 <vaniamaciel99@gmail.com>
2015-11-17 18:26:14 +0000

    ahhhhhhh i really hope i did okay on this git statusgit status

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -1,16 +1,28 @@
 #create a string and store it in a variable
+string="this is really confusing and i hope i get a good grade on this !!!"
 
 #print the value of the variable you just created
+puts string
 
 #Use ruby to calculate and print the sum of 185 and 14
+    sum=185 + 14
+puts sum
 
 #Use ruby to calculate and print the product of 1.8 and 3.6
+    product=1.8 * 3.6
+puts product
 
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
+    total=155 + 65 / 3.2
+puts total    
 
 #store the number 78 as a variable
+    variable="78"
+puts variable
 
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
+    average=78 + 55 + 63 + 98 / 4 
+puts average
 
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+# puts "When you run this file, this sentence should not appear on the terminal."
