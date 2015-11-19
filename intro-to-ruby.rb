072219a5947166dc4ee078efdf83c36a9098ef6a
gaotina <tqgsmailbox@gmail.com>
2015-11-19 14:58:24 +0000

    What are we doing tmr?

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -1,19 +1,17 @@
 #create a string and store it in a variable
-S="lol"
+
 
 #print out the value of the variable you just created
-puts S
+
 #print the sum of 185 and 14
-puts [185,14].sum
+
 #print the product of 1.8 and 3.6
 puts 1.8*3.6
 #store the number 78 as a variable
-y=78
+
 
 #print the average of the following numbers: 78, 55, 63, 98
-values = [78,55,63,98]
-total=4
-average = total/values.size
-puts average
+
+
 #comment out the following line
-rem "When you run this file, this sentence should not appear on the terminal."
+#puts "When you run this file, this sentence should not appear on the terminal."
