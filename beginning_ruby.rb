7b00b57534ad4c37a1dbcced4a4a7ca90dcd2777
breannc <bjcastillo1622@gmail.com>
2015-11-19 18:17:07 +0000

    Ana is the most amazing person in the world

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -1,16 +1,23 @@
 #create a string and store it in a variable
-
-#print the value of the variable you just created
-
+def string_stinger(string1)
+  puts string1.reverse
+end  
+  string_stinger("Breann")
+  
+  
+  
 #Use ruby to calculate and print the sum of 185 and 14
+puts 185*14
 
 #Use ruby to calculate and print the product of 1.8 and 3.6
-
+ puts 1.8*3.6
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
+puts ( 155 + 65 )/32
 
 #store the number 78 as a variable
-
+x=78
+puts x
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
-
+puts ( x + 55 + 63 + 98 ) / 4
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+# puts "When you run this file, this sentence should not appear on the terminal.
