c0a2f738b73b0047fa03fe5ad8a5a4cc9aa938ce
ehsherwoodc <ehsherwoodc@gmail.com>
2015-11-19 14:58:51 +0000

    This is the best test evergit statusgit statusgit statusgit statusgit statusgit status

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -1,24 +1,26 @@
 #create a string and store it in a variable
+
 y= "chocolate"
 x = "I love #{y} so much."
-#print out the value of the variable you just created
-puts x
-#print the sum of 185 and 14
-a= (185)
-b= (14)
 
-puts (a+b)
+#print out the value of the variable you just created
+
+puts x
+
+#print the sum of 185 and 14
+
+puts 185+14
+
 #print the product of 1.8 and 3.6
-c= (1.8)
-d= (3.6)
-puts (c*d)
+
+puts 1.8*3.6
 
 #store the number 78 as a variable
-z= (78)
+z= 78
 
 #print the average of the following numbers: 78, 55, 63, 98
 
-puts ((z+55+63+98)/(4))
+puts (z+55+63+98)/4
 
 #comment out the following line
 #puts "When you run this file, this sentence should not appear on the terminal."
