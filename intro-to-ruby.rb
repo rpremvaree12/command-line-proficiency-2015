f5d4d49ab8e1e9cde661ccaa8added45db574c85
paindrain084 <ncolon968@gmail.com>
2015-11-19 14:57:39 +0000

    WOOP

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -5,16 +5,16 @@ puts "#{soda}"
 puts soda.length
 #print the sum of 185 and 14
 
-print 185 + 14
+puts 185 + 14
 #print the product of 1.8 and 3.6
 
-print 1.8 * 3.6
+puts 1.8 * 3.6
 #store the number 78 as a variable
 
 number = 78
 #print the average of the following numbers: 78, 55, 63, 98
 
-print 78 + 55 + 63 + 98/ 4
+puts 78 + 55 + 63 + 98/ 4
 #comment out the following line
 
 # puts "When you run this file, this sentence should not appear on the terminal."#
