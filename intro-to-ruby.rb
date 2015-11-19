0cd199c74b15c87fd6aa462d52e3ae6f175190d6
thecleverwolf <ryan.m.bonilla@gmail.com>
2015-11-19 14:57:48 +0000

    test

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -12,7 +12,8 @@ puts (185+14)
 #print the product of 1.8 and 3.6
 puts (1.8*3.6)
 #store the number 78 as a variable
-
+x = 78
+puts "#{x}"
 #print the average of the following numbers: 78, 55, 63, 98
 puts (78+55+63+98)/4
 
