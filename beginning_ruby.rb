686d595a4856466f552fd824646c00b5704adb43
briannawatson <briannawatson61@gmail.com>
2015-11-19 18:16:17 +0000

    pull request

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -1,16 +1,19 @@
 #create a string and store it in a variable
-
+smoothies = [" strawberry", " blueberry"," mango", " pineapple"]
 #print the value of the variable you just created
-
+puts smoothies
 #Use ruby to calculate and print the sum of 185 and 14
+puts "#{184+14}"
 
 #Use ruby to calculate and print the product of 1.8 and 3.6
-
+puts "#{1.8 * 3.6}"
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
-
+puts (155 + 65) / (3.2)
 #store the number 78 as a variable
-
+variable = [ "#{79}"]
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
+puts average = ( 78 + 55 + 63 + 98) / (4)
+
 
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+#puts "When you run this file, this sentence should not appear on the terminal."
