a7e92450b66b878b0a2ac22a83f99fd921dec792
myaralkhulaqi <malkhulaqi24@gmail.com>
2015-11-19 18:20:12 +0000

    hello

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -16,7 +16,7 @@ puts 1.8*3.6
 
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
 
-puts (155*65)/3.2
+puts (155*65)/3.2 
 
 #store the number 78 as a variable
 
@@ -28,3 +28,5 @@ print (78*55*63*98)/4
 
 #comment out the following line
 #puts "When you run this file, this sentence should not appear on the terminal."
+
+puts (90+100+100+100+93+93)/6
