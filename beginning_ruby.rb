c6da415c535a16f32dbb06c73216c4024806e377
deniskabiesia <gdenise02@gmail.com>
2015-11-19 18:12:08 +0000

    hi

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -8,11 +8,11 @@ puts(185 + 140)
 #Use ruby to calculate and print the product of 1.8 and 3.6
 puts(1.8 * 3.6)
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
-puts(155 + 65)/(3.2)
+puts 155 + 65 / 3.2
 #store the number 78 as a variable
 m=78 
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
-puts average(78,55,63,98)
+puts 78 + 55 + 63 + 98 / 4
 
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+# puts "When you run this file, this sentence should not appear on the terminal."
