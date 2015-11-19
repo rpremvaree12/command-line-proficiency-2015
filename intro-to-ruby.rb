e76d348684546a517994e281faaf57a7075452c3
tfermin <tkfermin@gmail.com>
2015-11-19 15:20:02 +0000

    Hi Mr. Premvaree 2.0

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -3,13 +3,13 @@ name = "Hello, I'm Tiffany Fermin"
 #print out the value of the variable you just created
 puts name
 #print the sum of 185 and 14
-puts (185+14)
+puts 185+14
 
 #print the product of 1.8 and 3.6
-puts (1.8*3.6)
+puts 1.8*3.6
 #store the number 78 as a variable
 x = 78
 #print the average of the following numbers: 78, 55, 63, 98
-puts [(x+55+63+98)/4]
+puts (x+55+63+98)/4
 #comment out the following line
 # puts "When you run this file, this sentence should not appear on the terminal."
