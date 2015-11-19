b7b448d43a469c96b1683223b820d2b0dc2ee650
ypeguero27 <yamy2765@gmail.com>
2015-11-19 18:15:53 +0000

    I want food

--- beginning_ruby.rb
+++ beginning_ruby.rb
@@ -1,16 +1,19 @@
-#create a string and store it in a variable
 
+y = "food"
+puts y 
+puts "I love #{y}"
 #print the value of the variable you just created
-
+puts y 
 #Use ruby to calculate and print the sum of 185 and 14
-
+puts 185 + 14 
 #Use ruby to calculate and print the product of 1.8 and 3.6
-
+puts 1.8 * 3.6
 #Use ruby to calculate the total of when you add 155 to 65 and divide it by 3.2
-
-#store the number 78 as a variable
-
+puts (65+ 155) /3.2
+#store the number 78 as a variable`
+k = 78
+puts k
 #Use ruby to print the average of the following numbers: 78, 55, 63, 98
-
+puts (78 + 55 + 63 + 98) / 4
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+#"When you run this file, this sentence should not appear on the terminal."
