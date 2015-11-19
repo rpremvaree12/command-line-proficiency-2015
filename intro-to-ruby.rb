710138bf76c332e02b763176184a8992e7f1eccb
Kevin Wang <wang.kevin718@gmail.com>
2015-11-19 15:20:16 +0000

    I added to this!

--- intro-to-ruby.rb
+++ intro-to-ruby.rb
@@ -3,15 +3,12 @@ Hello ="Sir!"
 #print out the value of the variable you just created
 puts "Hey! #{Hello}"
 #print the sum of 185 and 14
-puts (185+14)sum
+puts "#{185+14}"
 #print the product of 1.8 and 3.6
-puts 1.8 + 3.6
+puts "#{1.8 + 3.6}"
 #store the number 78 as a variable
-y=78
+y="#{78}"
 #print the average of the following numbers: 78, 55, 63, 98
-values = 78,55,63,98
-total = 298
-average = 73.5
-puts average
+puts "#{78+55+63+98/4}" 
 #comment out the following line
-puts "When you run this file, this sentence should not appear on the terminal."
+#puts "When you run this file, this sentence should not appear on the terminal."
